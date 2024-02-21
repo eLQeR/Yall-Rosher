0ba77332c86f68eb2a4f6382f73c91decd7419a9
Max Bondarenko <jaderystudion@gmail.com>
2024-02-21 22:51:23 +0200

    Styled dropdowns

--- frontend/components/TextWithDropdown.jsx
+++ frontend/components/TextWithDropdown.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
+import './TextWithDropdown.css'
 
 const TextWithDropdown = ({ text, DropdownComponent }) => {
     const [showDropdown, setShowDropdown] = useState(false)
 
     return (
         <span
+            className="text"
             onMouseEnter={() => setShowDropdown(true)}
             onMouseLeave={() => setShowDropdown(false)}
         >
             {text}
-            {showDropdown ? <DropdownComponent /> : ''}
+            {showDropdown ? (
+                <div className="dropdown">
+                    <DropdownComponent />
+                </div>
+            ) : (
+                ''
+            )}
         </span>
     )
 }
--- frontend/components/dropdowns/New.jsx
+++ frontend/components/dropdowns/New.jsx
@@ -1,8 +1,48 @@
 const NewDropdown = () => {
     return (
-        <ul>
-            <li>Aboba</li>
-        </ul>
+        <div className="wrapper">
+            <div className="column">
+                <p className="title">Одяг</p>
+                <ul>
+                    <li>Новинки</li>
+                    <li>Світшоти</li>
+                    <li>Джинси</li>
+                    <li>Футболки</li>
+                    <li>Штани</li>
+                    <li>Куртки, пальта</li>
+                    <li>Сукні, комбінезони</li>
+                    <li>Сорочки, блузки</li>
+                    <li>Спідниці</li>
+                    <li>Светри</li>
+                    <li>Набори</li>
+                </ul>
+            </div>
+            <div className="column">
+                <p>Взуття</p>
+                <ul>
+                    <li>Побачити все</li>
+                    <li>Ботильйони, черевики</li>
+                    <li>Взуття, еспадрільї</li>
+                    <li>Кросівки</li>
+                    <li>На високих підборах</li>
+                </ul>
+            </div>
+            <div className="column">
+                <p>Аксесуари</p>
+                <ul>
+                    <li>Побачити все</li>
+                    <li>Сумки, рюкзаки</li>
+                    <li>Піжами, капці</li>
+                    <li>Шкарпетки</li>
+                    <li>Шапки</li>
+                    <li>Жіноча білизна</li>
+                    <li>Колготки</li>
+                    <li>Пояси, гаманці</li>
+                    <li>Біжутерія</li>
+                    <li>Інше</li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
