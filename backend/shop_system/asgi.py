f5bc8d8a9124197bab6f4b146077f03021a284c0
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-05-19 22:52:22 +0300

    Rename project

--- backend/shop_system/asgi.py
+++ backend/shop_system/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for shop_system project.
+ASGI config for backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- backend/shop_system/wsgi.py
+++ backend/shop_system/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for shop_system project.
+WSGI config for backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
