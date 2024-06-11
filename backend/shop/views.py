cc53d38d61cec10b7d745bdc316ef3e2796c0605
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-06-11 12:17:46 +0300

    Add swagger documentation to Shop app

--- backend/shop/views.py
+++ backend/shop/views.py
@@ -1,13 +1,12 @@
 from django.core.exceptions import FieldError
 from django.shortcuts import get_object_or_404
-from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample, extend_schema_view
 from rest_framework import viewsets, generics, mixins, status
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.exceptions import ValidationError
-
 
 from shop.models import Item, SemiCategory, Order, VariantOfItem, OrderItem
 from shop.permissions import IsAdminOrReadOnly, CanCancelOrCreateOrGet
@@ -18,10 +17,39 @@ from shop.serializers import (
     OrderSerializer,
     VariantOfItemSerializer,
     ItemDetailSerializer,
-    VariantOfItemListSerializer, OrderDetailSerializer, OrderListSerializer,
+    VariantOfItemListSerializer,
+    OrderDetailSerializer,
+    OrderListSerializer,
+    ItemCreateUpdateSerializer,
 )
 
 
+@extend_schema_view(
+    list=extend_schema(
+        summary="Get list of your semi categories",
+        description="User can get a list of semi categories.",
+    ),
+    retrieve=extend_schema(
+        summary="Get a certain semi category",
+        description="User can get a certain semi category.",
+    ),
+    create=extend_schema(
+        summary="Create a certain semi category",
+        description="Admin can create a semi category.",
+    ),
+    update=extend_schema(
+        summary="Update a certain semi category",
+        description="Admin can update a semi category.",
+    ),
+    partial_update=extend_schema(
+        summary="Partial update a certain semi category",
+        description="Admin can make partial update a semi category.",
+    ),
+    destroy=extend_schema(
+        summary="Delete a certain semi category",
+        description="Admin can delete a semi category.",
+    ),
+)
 class SemiCategoryViewSet(viewsets.ModelViewSet):
     queryset = SemiCategory.objects.all()
     serializer_class = SemiCategorySerializer
@@ -42,6 +70,32 @@ class MediumResultsSetPagination(PageNumberPagination):
     max_page_size = 20
 
 
+@extend_schema_view(
+    list=extend_schema(
+        summary="Get list of your items",
+        description="User can get a list of items.",
+    ),
+    retrieve=extend_schema(
+        summary="Get a certain item",
+        description="User can get a certain item.",
+    ),
+    create=extend_schema(
+        summary="Create a certain item",
+        description="Admin can create an item.",
+    ),
+    update=extend_schema(
+        summary="Update a certain item",
+        description="Admin can update an item.",
+    ),
+    partial_update=extend_schema(
+        summary="Partial update a certain item",
+        description="Admin can make partial update an item.",
+    ),
+    destroy=extend_schema(
+        summary="Delete a certain item",
+        description="Admin can delete an item.",
+    ),
+)
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
@@ -53,8 +107,7 @@ class ItemViewSet(viewsets.ModelViewSet):
             return ItemListSerializer
         if self.action == "retrieve":
             return ItemDetailSerializer
-
-        return ItemSerializer
+        return ItemCreateUpdateSerializer
 
     def get_queryset(self):
 
@@ -82,7 +135,66 @@ class ItemViewSet(viewsets.ModelViewSet):
                 )
         return queryset
 
+    @extend_schema(
+        summary="Get list of items",
+        description="User can get a list of items that are available.",
+        methods=["GET"],
+        parameters=[
+            OpenApiParameter(
+                name="category",
+                description="Filter by id of semi-category.",
+                required=False,
+                type=str,
+            ),
+            OpenApiParameter(
+                name="name",
+                description="Filter by name",
+                required=False,
+                type=str
+            ),
+            OpenApiParameter(
+                name="ordering",
+                description="Ordering items",
+                required=False,
+                type=str
+            ),
+        ],
+        examples=[
+            OpenApiExample(
+                "Example 1",
+                value={
+                    "category": 4,
+                    "name": "Світшот",
+                }
+            )
+        ],
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
+
+@extend_schema_view(
+    retrieve=extend_schema(
+        summary="Get a certain variant of item",
+        description="User can get a certain variant of item.",
+    ),
+    create=extend_schema(
+        summary="Create a certain variant of item",
+        description="Admin can create a variant of item.",
+    ),
+    update=extend_schema(
+        summary="Update a certain variant of item",
+        description="Admin can update a variant of item.",
+    ),
+    partial_update=extend_schema(
+        summary="Partial update a certain variant of item",
+        description="Admin can make partial update a variant of item.",
+    ),
+    destroy=extend_schema(
+        summary="Delete a certain variant of item",
+        description="Admin can delete a variant of item.",
+    ),
+)
 class VariantOfItemViewSet(viewsets.ModelViewSet):
     queryset = VariantOfItem.objects.all()
     serializer_class = VariantOfItemSerializer
@@ -156,6 +268,25 @@ class LowResultsSetPagination(PageNumberPagination):
     max_page_size = 10
 
 
+@extend_schema_view(
+    list=extend_schema(
+        summary="Get list of your orders",
+        description="User can get a list of his orders.",
+    ),
+    retrieve=extend_schema(
+        summary="Get a certain order",
+        description="User can get a certain his order.",
+    ),
+    create=extend_schema(
+        summary="Create a certain order",
+        description="User can create an order.",
+    ),
+    cancel_order=extend_schema(
+        summary="Cancel a certain order",
+        description="User can cancel a certain his order.",
+        methods=["POST"],
+    ),
+)
 class OrderViewSet(
     mixins.ListModelMixin,
     mixins.CreateModelMixin,
@@ -184,11 +315,11 @@ class OrderViewSet(
     def get_queryset(self):
         queryset = self.queryset
         if self.action == "list":
-           queryset = queryset.select_related().prefetch_related(
+            queryset = queryset.select_related().prefetch_related(
                 "items",
                 "items__item",
             )
-        return queryset.filter(user=self.request.user).select_related('user').prefetch_related('items', 'items__item')
+        return queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
         if self.action == 'retrieve':
