23a7757825f82fd947ec4ff1fbaa972d1110f53f
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-06-11 12:14:58 +0300

    remove token urls from shop application

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path, include
 from shop.views import SemiCategoryViewSet, ItemViewSet, OrderViewSet, VariantOfItemViewSet
 from rest_framework import routers
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 router = routers.DefaultRouter()
 
@@ -15,8 +11,6 @@ router.register("variants_of_item", VariantOfItemViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
 app_name = "shop"
